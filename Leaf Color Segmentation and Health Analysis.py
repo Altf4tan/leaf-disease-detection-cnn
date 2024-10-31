1e2ebb3dc9289a0615474078baf0364f856b785d
Altan Basoglu <59366678+Altf4tan@users.noreply.github.com>
2024-10-31 13:36:04 +0300

    Update Leaf Color Segmentation and Health Analysis.py
    translated comments from turkish to english

--- Leaf Color Segmentation and Health Analysis.py
+++ Leaf Color Segmentation and Health Analysis.py
@@ -1,29 +1,29 @@
 import cv2
 import numpy as np
 
-# Görüntüyü oku
+# read the image
 img = cv2.imread('C:\\Users\\MSI\\Desktop\\leaf2.jpg', 1)
 
-# RGB'yi HSV'ye dönüştür
+# Translate RGB to HSV
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# Yeşil için HSV renk aralığı
+# HSV array for the color green
 lower_green = np.array([35, 100, 100])
 upper_green = np.array([85, 255, 255])
 
-# Sarı/Kahverengi için HSV renk aralığı
+# HSV array for brown and yellow
 lower_brown = np.array([10, 100, 20])
 upper_brown = np.array([20, 255, 200])
 
-# Renk aralığına sahip pikselleri tespit et
+# Identify pixels that have those colors
 mask_green = cv2.inRange(hsv, lower_green, upper_green)
 mask_brown = cv2.inRange(hsv, lower_brown, upper_brown)
 
-# Maskeleme ile orijinal görüntü üzerindeki yeşil ve kahverengi alanları bul
+# Find green and brown areas on original image with masking
 result_green = cv2.bitwise_and(img, img, mask=mask_green)
 result_brown = cv2.bitwise_and(img, img, mask=mask_brown)
 
-# Sonuç görüntülerini göster
+# Show the result images
 cv2.imshow('Original image', img)
 cv2.imshow('Detected green color', result_green)
 cv2.imshow('Detected brown color', result_brown)
